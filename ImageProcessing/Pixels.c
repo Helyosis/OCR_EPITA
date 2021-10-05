f0b48e09f4e3ce0217b9cc44c1808a7861f4c4dd
Aloïs Colléaux-Le Chêne <alois56200@gmail.com>
2021-10-05 11:29:10 +0200

    Added Drawline() function to draw lines on SDL_Surface objects

--- ImageProcessing/Pixels.c
+++ ImageProcessing/Pixels.c
@@ -1,10 +1,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <math.h>
 
-/*
- * Return the pixel value at (x, y)
- * NOTE: The surface must be locked before calling this!
- */
+
 Uint32 getPixel(SDL_Surface *surface, int x, int y)
 {
     int bpp = surface->format->BytesPerPixel;
@@ -32,10 +30,7 @@ Uint32 getPixel(SDL_Surface *surface, int x, int y)
     }
 }
 
-/*
- * Set the pixel at (x, y) to the given value
- * NOTE: The surface must be locked before calling this!
- */
+
 void putPixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 {
     int bpp = surface->format->BytesPerPixel;
@@ -69,7 +64,25 @@ void putPixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
     }
 }
 
-// Returns intensity of the point (x, y) in source
 int I(SDL_Surface *source, int x, int y) {
     return getPixel(source, x, y) & 0xFF;
+}
+
+void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, Uint32 pixel) {
+
+	int i;
+    double x = x1 - x0; 
+	double y = y1 - y0; 
+	double length = sqrt( x*x + y*y ); 
+	double addx = x / length; 
+	double addy = y / length; 
+	x = x0; 
+	y = y0; 
+	
+	for ( i = 0; i < length; i += 1) { 
+		putpixel(Screen, x, y, pixel ); 
+		x += addx; 
+		y += addy; 
+		
+	} 
 }
