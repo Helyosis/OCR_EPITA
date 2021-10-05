6d2cb08f6d8dac7a2b7937c04f9dacbdf72eec89
Antabuse-does-something <Antabuseyolo@gmail.com>
2021-10-05 11:22:29 +0200

    Started : Image manipulation

---
+++ image-manipulation/manipulation.c
@@ -0,0 +1,11 @@
+#include <SDL2.h>
+
+// Image manipulation functions
+
+//Rotation, rotate an image in place
+//@param image: the image to rotate
+//@param angle: the angle to rotate the image by
+void rotate(SDL_Surface *image, double angle){
+
+
+}
---
+++ image-manipulation/manipulation.h
@@ -0,0 +1,6 @@
+#include <SDL2.h>
+#ifndef MANIPULATION_H
+#define MANIPULATION_H
+void rotation(SDL_Surface *image, int angle);
+
+#endif
