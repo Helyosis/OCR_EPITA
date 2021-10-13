a43e0986ee8550896819318c487ca1504e2ce642
Alois Colleaux-Le Chene <alois.colleaux-le-chene@epita.fr>
2021-10-13 11:05:08 +0200

    Refactor code

--- src/ImageProcessing/Pixels.h
+++ src/ImageProcessing/Pixels.h
@@ -3,7 +3,11 @@
 
 Uint32 getPixel(SDL_Surface *surface, int x, int y);
 void putPixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
+<<<<<<< HEAD:src/ImageProcessing/Pixels.h
 void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, uint32_t pixel);
 
+=======
+int I(SDL_Surface *source, int x, int y);
+>>>>>>> 0d24310 (Refactor code):ImageProcessing/Pixels.h
 
 #endif
