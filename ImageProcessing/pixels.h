a7de586fac4859ee349bc885fa7c763def029a00
Aloïs Colléaux--Le Chêne <alois.colleaux-le-chene@epita.fr>
2021-10-18 10:57:42 +0200

    Added definition of drawLine() in header

--- ImageProcessing/pixels.h
+++ ImageProcessing/pixels.h
@@ -1,26 +1,9 @@
 #ifndef PIXELS_H
 #define PIXELS_H
 
-/*
- * Return the pixel value at (x, y)
- * NOTE: The surface must be locked before calling this!
- */
 Uint32 getPixel(SDL_Surface *surface, int x, int y);
-
-/*
- * Set the pixel at (x, y) to the given value
- * NOTE: The surface must be locked before calling this!
- */
 void putPixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
+void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, uint32_t pixel);
 
-/*
- * Returns the intensity (in grayscale value) of 
- */
-int I(SDL_Surface *source, int x, int y);
-
-/*
- * Draw line in place in SDL_Surface, from point (x0, y0) to (x1, y1) using the same pixel colro
- */
-void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, Uint32 pixel);
 
 #endif
