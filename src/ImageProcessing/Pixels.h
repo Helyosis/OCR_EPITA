3ce64686d8c442813bc7744aab256589fc3cbc02
Antabuse-does-something <antabuseyolo@gmail.com>
2021-10-27 18:45:50 +0200

    Conflict

--- src/ImageProcessing/Pixels.h
+++ src/ImageProcessing/Pixels.h
@@ -3,15 +3,8 @@
 
 Uint32 getPixel(SDL_Surface *surface, int x, int y);
 void putPixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
-<<<<<<< HEAD:src/ImageProcessing/Pixels.h
 void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, uint32_t pixel);
-
-=======
 int I(SDL_Surface *source, int x, int y);
-<<<<<<< HEAD:src/ImageProcessing/Pixels.h
->>>>>>> 0d24310 (Refactor code):ImageProcessing/Pixels.h
-=======
 void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, uint32_t pixel);
->>>>>>> 10147ee (Continued thinggsss):ImageProcessing/Pixels.h
 
 #endif
