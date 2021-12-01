2a609bcb18d0f304d076e1e91e9f611573a4edde
Kristen Quesnel <kristen.quesnel@epita.fr>
2021-12-01 11:49:23 +0100

    Started : Omographic transphorm

---
+++ src/ImageProcessing/OmographicTransphorm.h
@@ -0,0 +1,8 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#include <SDL.h>
+#include <SDL_image.h>
+#pragma GCC diagnostic pop
+
+#include "Pixels.h"
