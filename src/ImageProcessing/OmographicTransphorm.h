906bd680613580799d79574c09569a7e97721083
Kristen Quesnel <kristen.quesnel@epita.fr>
2021-12-02 09:45:39 +0100

    Implemented : Matrix opperations

--- src/ImageProcessing/OmographicTransphorm.h
+++
@@ -1,8 +0,0 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-#pragma GCC diagnostic ignored "-Wtype-limits"
-#include <SDL.h>
-#include <SDL_image.h>
-#pragma GCC diagnostic pop
-
-#include "Pixels.h"
