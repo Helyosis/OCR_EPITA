7d9575dec116a3081e494423538a29c0e4a18500
Giovanni Le Bail <lebailgiovanni@epita.fr>
2021-12-07 10:20:01 +0100

    Fix

--- src/ImageProcessing.c
+++ src/ImageProcessing.c
@@ -28,55 +28,31 @@ int processImage(char* in_filename, char* out_filename) {
         errx(1, "Error");
     }
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("My GUI lol",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          image->w, image->h, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
     printf("[+] Successfully loaded %s (w=%d, h=%d)\n",
            in_filename, image->w, image->h);
-    //displaySurface(renderer, image);
     Apply_grayscale_filter(image);
-    //displaySurface(renderer, image);
     printf("[*] Applied grayscale\n");
 
-    //wait_for_keypressed();
     GaussianBlur_inPlace(image);
-    //displaySurface(renderer, image);
-    //wait_for_keypressed();
-    //displaySurface(renderer, image);
     printf("[*] Reduced noise\n");
 
     CannyFilter_inPlace(image);
-    //AdaptiveThresholding_inPlace(image);
     printf("[*] Applied adaptive threshold (mean - C method)\n");
     
-    displaySurface(renderer, image);
-
-    //wait_for_keypressed();
     houghTransform_result* res = HoughTransform(image);
     DrawHoughlines(image, res);
 
 
-    //displaySurface(renderer, image);
     free_houghTransform_result(res);
 
     printf("[+] Drew Hough lines\n");
-//printf("[-] Hough transform is not implemented yet. Skipping.\n");
     printf("[-] Perspective transformation is not implemented yet. Skipping.\n");
 
-    //wait_for_keypressed();
     image = Rotation_shearing(image,105);
-    //displaySurface(renderer, image);
-    SDL_SaveBMP(image, out_filename);
+    IMG_SavePNG(image, out_filename);
 
     printf("Saved images !\n");
-
-    //wait_for_keypressed();
-
-    SDL_FreeSurface(image);
 
     SDL_Quit();
     IMG_Quit();
