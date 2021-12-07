78c8efe0706a4f2e9421152ce1b373794d95ffa6
Tudual Lefeuvre <tudual.lefeuvre@epita.fr>
2021-12-07 17:46:30 +0100

    fix

--- src/NeuralNetwork/NeuralNet.h
+++ src/NeuralNetwork/NeuralNet.h
@@ -5,4 +5,5 @@ void feedForward(struct NeuralNetwork* nnPtr);
 void sigmoid(double* mA,double* m, int width);
 void saveNn(char* fileName, struct NeuralNetwork* nnPtr);
 struct NeuralNetwork* loadNn(char* fileName);
+double* predict(char* imageFileName, char* SavefileName);
 #endif
